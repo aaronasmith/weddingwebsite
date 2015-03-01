c15cd5ad92a1d2ae1103ccb2596faff69d62fb91
Aaron Smith <aaronasmith@gmail.com>
2015-02-28 18:07:29 -0600

    Initial commit

---
+++ WeddingWebsite/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Story()
+        {
+            return View();
+        }
+
+        public ActionResult Party()
+        {
+            return View();
+        }
+
+        public ActionResult Interest()
+        {
+            return View();
+        }
+
+        public ActionResult Registry()
+        {
+            return View();
+        }
+    }
+}
---
+++ WeddingWebsite/MenuItemHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingWebsite
+{
+    public static class MenuItemHelper
+    {
+        public static MvcHtmlString ListItemAction(this HtmlHelper helper, string name, string actionName)
+        {
+            var currentControllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            var currentActionName = (string)helper.ViewContext.RouteData.Values["action"];
+
+            var menuItem = new TagBuilder("li");
+            var url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            menuItem.InnerHtml = String.Format("<a href=\"{0}\"{2}>{1}</a>", url.Action(actionName), name, currentActionName == actionName ? " class=\"active\"" : "");
+            return new MvcHtmlString(menuItem.ToString());
+        }
+    }
+}
