7823d9487c793f747a4d4846fd8b09693e004cef
Aaron Smith <aaronasmith@gmail.com>
2015-03-03 21:30:00 -0600

    Changes

--- WeddingWebsite/Scripts/custom.js
+++ WeddingWebsite/Scripts/custom.js
@@ -31,7 +31,7 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-var date = new Date(2015, 10, 23);
+var date = new Date(2015, 10, 23, 17, 30 );
 var now = new Date();
 var diff = (date.getTime() / 1000) - (now.getTime() / 1000);
 
